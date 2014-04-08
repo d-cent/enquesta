612a342b74beee42329ff6615b99bdfe1f5850b3
Brian Flanagan <b@flngn.com>
2014-04-08 15:37:45 +0100

    sort user hash components to ensure consistency

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
   def create
     @poll = Poll.find_securely(params[:poll_id])
     option = @poll.options.where(id: vote_params[:option_id]).first
-    user_hash_components = [params[:poll_id], request.env['REMOTE_ADDR'], request.env['HTTP_USER_AGENT'], request.env['HTTP_ACCEPT']].reject { |c| c.nil? }.join('')
+    user_hash_components = [params[:poll_id], request.env['REMOTE_ADDR'], request.env['HTTP_USER_AGENT'], request.env['HTTP_ACCEPT']].sort.reject { |c| c.nil? }.join('')
     user_hash = Digest::MD5.hexdigest(user_hash_components)
     vote = Vote.new(option: option, user_hash: user_hash)
     
