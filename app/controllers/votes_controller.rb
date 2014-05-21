f568d8d9f5b8467997fda997475190e9f31824fa
Brian Flanagan <b@flngn.com>
2014-05-21 12:19:15 +0100

    mild code reformatting for clarity

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,17 +2,14 @@ class VotesController < ApplicationController
   def create
     poll = Poll.find_securely(params[:poll_id])
     option = poll.options.where(id: vote_params[:option_id]).first
+    Vote.create(option: option, user_hash: current_user_hash_for_poll(poll))
     
-    vote = Vote.new(option: option, user_hash: current_user_hash_for_poll(poll))
-    
-    vote.save
     redirect_to poll
   end
   
   def update
     poll = Poll.find_securely(params[:poll_id])
     option = poll.options.where(id: vote_params[:option_id]).first
-  
     vote = poll.votes.where(user_hash: current_user_hash_for_poll(poll)).first
     
     vote.update(option: option)
